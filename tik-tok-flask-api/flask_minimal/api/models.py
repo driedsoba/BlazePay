56840273a155ee1cb5d96195c9557d351dfdb50b
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2023-09-04 22:29:22 +0800

    added create transaction endpoint

--- tik-tok-flask-api/flask_minimal/api/models.py
+++ tik-tok-flask-api/flask_minimal/api/models.py
@@ -1,36 +1,77 @@
-from flask_restful import Resource, Api, fields, abort
+from flask_restful import Resource, Api, fields, abort, reqparse
 from flask import request
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, db
+
+## cred initialization
 cred = credentials.Certificate(
-    '/Users/rickywinarko/Documents/Github/TikTok/TIK-TOK-FLASK-API/credentials.json')
+    '/Users/victorlim/Documents/projects/TikTok/tik-tok-flask-api/credentials.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+## request parser for new endpoint
+transaction_parser = reqparse.RequestParser()
+transaction_parser.add_argument('sender', type=str, required=True, help='Sender is required')
+transaction_parser.add_argument('receiver', type=str, required=True, help='Receiver is required')
+transaction_parser.add_argument('amount', type=float, required=True, help='Amount is required')
 
 class DummyResult(Resource):
     def get(self):
-        # ref = db.reference("/E-Wallet")
-        # ref.set({
-        #     "Books":
-        #     {
-        #         "Best_Sellers": -1
-        #     }
-        # })
-
-        ref = db.document("users/z7zbK5xAnh9ORgj68Q4m")
-
-        # with open("book_info.json", "r") as f:
-        #     file_contents = json.load(f)
-
-        # for key, value in file_contents.items():
-        # ref.set({"dummy": "test"})
-        return {"dummy": ref.get().to_dict()}
-
+        users_ref = db.collection("users")
+        users = users_ref.stream()
+        for user in users:
+            print(f"Document ID: {user.id}")
+            print(f"Data: {user.to_dict()}")
+        return {"dummy": "mee"}
 
 class HelloResult(Resource):
     def post(self):
         json_data = request.get_json(force=True)
         name = json_data["name"]
         return {"greetings": "Hello {}".format(name)}
+
+class CreateTransaction(Resource):
+    def post(self):
+        try:
+            args = transaction_parser.parse_args()
+            sender = args['sender']
+            receiver = args['receiver']
+            amount = args['amount']
+
+            users_ref = db.collection('users')
+            sender_doc = users_ref.document(sender).get()
+            receiver_doc = users_ref.document(receiver).get()
+
+            if not sender_doc.exists or not receiver_doc.exists:
+                return {'error': 'Sender or receiver does not exist'}, 404
+            
+            sender_transactions_ref = db.collection('transactions')
+            sender_transaction_ref = sender_transactions_ref.add({
+                'receiver_user_id': sender,
+                'sender_user_id': receiver,
+                'amount': -amount,  # Debit amount is negative
+                'type': 'debit' 
+            })
+            
+            receiver_transactions_ref = db.collection('transactions')
+            receiver_transaction_ref = receiver_transactions_ref.add({
+                'receiver_user_id': receiver,
+                'sender_user_id': receiver,
+                'amount': amount,  # Debit amount is negative
+                'type': 'debit' 
+            })
+
+            # Update user balances using double-entry accounting
+            sender_balance = sender_doc.get('balance')
+            receiver_balance = receiver_doc.get('balance')
+
+            sender_doc.reference.update({'balance': sender_balance - amount})
+
+            # Credit receiver's account
+            receiver_doc.reference.update({'balance': receiver_balance + amount})
+
+            return {'message': 'Transfer transaction created successfully'}, 201
+        except Exception as e:
+            return {"error": f"error happened at: {e}"}
+
--- tik-tok-flask-api/flask_minimal/server.py
+++ tik-tok-flask-api/flask_minimal/server.py
@@ -1,18 +1,15 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-from flask_minimal.api.models import DummyResult
-from flask_minimal.api.models import HelloResult
+from flask_minimal.api.models import *
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(DummyResult, '/dummy')
-api.add_resource(HelloResult, '/hello')
-
+api.add_resource(CreateTransaction, '/hello')
 
 def run_app(*args, **kwargs):
-    app.run(*args, **kwargs)
-
+    app.run(*args, **kwargs, port=3000)
 
 if __name__ == '__main__':
     app.run(debug=True)
