7d569213d6bab1f8fcf1e5d49c9c21bca25e58de
SpiderPigzz <Ricky.winarko@gmail.com>
2023-09-04 17:38:10 +0800

    Added connection to Firebase

--- tik-tok-flask-api/flask_minimal/api/models.py
+++ tik-tok-flask-api/flask_minimal/api/models.py
@@ -1,10 +1,32 @@
 from flask_restful import Resource, Api, fields, abort
 from flask import request
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate(
+    '/Users/rickywinarko/Documents/Github/TikTok/TIK-TOK-FLASK-API/credentials.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
 class DummyResult(Resource):
     def get(self):
-        return {"dummy": "test"}
+        # ref = db.reference("/E-Wallet")
+        # ref.set({
+        #     "Books":
+        #     {
+        #         "Best_Sellers": -1
+        #     }
+        # })
+
+        ref = db.document("users/z7zbK5xAnh9ORgj68Q4m")
+
+        # with open("book_info.json", "r") as f:
+        #     file_contents = json.load(f)
+
+        # for key, value in file_contents.items():
+        # ref.set({"dummy": "test"})
+        return {"dummy": ref.get().to_dict()}
 
 
 class HelloResult(Resource):
