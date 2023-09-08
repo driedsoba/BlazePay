4d313183b6d5e84ef61864bc8ce3d796b56d27e6
SpiderPigzz <Ricky.winarko@gmail.com>
2023-09-08 14:50:47 +0800

    Added in stripe top up functionality

--- tik-tok-flask-api/flask_minimal/api/models.py
+++ tik-tok-flask-api/flask_minimal/api/models.py
@@ -3,7 +3,7 @@ from flask import request
 import json
 import bcrypt
 import uuid
-
+import stripe
 # FIREBASE DEP
 import firebase_admin
 from firebase_admin import credentials, firestore, db
@@ -12,6 +12,9 @@ from firebase_admin import credentials, firestore, db
 cred = credentials.Certificate('../tik-tok-flask-api/credentials.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+stripe.api_key = 'sk_test_51NnkabDYjIOWSmdjO2B6ZGZEODYNO35Imq89RFYYJWN6hzY0PVEYc4U4b3Ph7R3fMyQBAsneHwMAsvEJrDUVowNH00Y1sjDiL9'
+
 
 class CreateUser(Resource):
     def post(self):
@@ -50,10 +53,13 @@ class CreateUser(Resource):
             return {"error": f"error happened at creating user, {e}"}
 
 # GET API to search for user document id
+
+
 class SearchUser(Resource):
-    def get(self):
+    def post(self):
         try:
-            search_query = request.args.get('user')
+            user_data = request.get_json(force=True)
+            search_query = user_data['user']
 
             if not search_query:
                 return {'error': 'Search query is required'}, 400
@@ -74,11 +80,14 @@ class SearchUser(Resource):
             return {'error': str(e)}, 500
 
 # maybe can add JWT and also take a look at security check/rule via firebase for double layer protection
+
+
 class GetProfile(Resource):
     def post(self):
         try:
-            phone = request.args.get('phone')
-            pin = request.args.get('pin')
+            login_data = request.get_json(force=True)
+            phone = login_data['phone']
+            pin = login_data['pin']
 
             if not (phone and pin):
                 return {'error': 'Phone and Pin query are required'}, 400
@@ -106,6 +115,97 @@ class GetProfile(Resource):
             return {'error': str(e)}, 500
 
 
+class CreateStripePayment(Resource):
+    def post(self):
+        try:
+            topup_parameters = request.get_json(force=True)
+
+            amount = topup_parameters['amount']
+            currency = topup_parameters['currency'].upper()
+
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+
+class stripe_webhook(Resource):
+    def post(self):
+        payload = request.get_data(as_text=True)
+        sig_header = request.headers.get("Stripe-Signature")
+        json_payload = json.loads(payload)
+
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, 'whsec_6e512dcf7620722751acbd679467d53df8d76cdf314ff95466712068f3fd466a'
+            )
+
+        except ValueError as e:
+            # Invalid payload
+            return "Invalid payload", 400
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return "Invalid signature", 400
+
+        # Handle the checkout.session.completed event
+        if event["type"] == "checkout.session.completed":
+            print("Payment was successful.")
+
+            user_ref = db.collection("users")
+            currency = json_payload['data']['object']['currency'].upper()
+
+            query = db.collection('transactions').where(
+                "stripe_session_id", "==", json_payload['data']['object']['id']).limit(1).stream()
+
+            if query:
+
+                for doc in query:
+                    print("Stripe transaction ID found!")
+                    doc_id = doc.id
+                    doc_object = doc.to_dict()
+                    print(doc_object)
+                    account = doc_object['transaction_acc']
+                    amount = doc_object['amount']
+
+                    # GET THE REFERENCE TO BE UPDATED
+                    doc_ref = db.collection(
+                        'transactions').document(doc_id).get()
+                    doc_ref.reference.update({"status": True})
+
+                    acc_doc = user_ref.document(account).get()
+                    prefix_to_search = [account]
+                    total_balance = {}
+                    for prefix in prefix_to_search:
+                        query = db.collection('transactions').where(
+                            "transaction_acc", "==", prefix).where("status", "==", True)
+                        total_amount = 0
+                        # Execute the query to retrieve matching documents
+                        docs = query.stream()
+                        # Iterate through the matching documents and access their "amount" field
+                        for doc in docs:
+                            data = doc.to_dict()
+                            if "amount" in data and data['currency'] == currency:
+                                amount = data["amount"]
+                                total_amount += amount
+
+                        total_balance[prefix] = total_amount
+
+                    print(currency)
+
+                    print(total_balance)
+
+                    acc_balance = acc_doc.get('balance').get(currency)
+                    print(acc_balance)
+
+                    if total_balance[account] == acc_balance+amount:
+                        acc_doc.reference.update(
+                            {'balance.' + currency: acc_balance + amount})
+                    break
+
+            else:
+                return {'error': 'Transaction not matched/found'}, 404
+
+        return "Success", 200
+
+
 class TopUp(Resource):
     def post(self):
         try:
@@ -121,48 +221,43 @@ class TopUp(Resource):
             if not acc_doc.exists:
                 return {'error': 'Account does not exist, check whether acc is still valid'}, 404
 
+            session = stripe.checkout.Session.create(
+                line_items=[{
+                    'price_data': {
+                        'currency': currency,
+                        'product_data': {
+                            'name': 'TopUp',
+                        },
+                        'unit_amount': amount,
+                    },
+                    'quantity': 100,
+                }],
+                mode='payment',
+                success_url='http://localhost:5000/success',
+                cancel_url='http://localhost:5000/cancel',
+            )
+
+            # return {'stripe_url': session.url, "sessionId": session["id"]}, 201
+
             # Iterate through the query results
-            account_transactions_ref = db.collection('transactions').document(account+'-'+transaction_id)
+            account_transactions_ref = db.collection(
+                'transactions').document(account+'-'+transaction_id)
             account_transactions_ref.set({
                 'transaction_acc': account,
                 'amount': amount,  # Debit amount is negative
                 'currency': currency,
                 'type': 'debit',
-                'mode':'topup',
-                'status':False
+                'mode': 'topup',
+                'stripe_session_id': session['id'],
+                'status': False
             })
 
-            prefix_to_search =[account]
-            total_balance={}
-            for prefix in prefix_to_search:
-                query = db.collection('transactions').where("transaction_acc", "==", prefix )
-                total_amount = 0
-                # Execute the query to retrieve matching documents
-                docs = query.stream()
-                # Iterate through the matching documents and access their "amount" field
-                for doc in docs:
-                    data = doc.to_dict()
-                    if "amount" in data and data['currency']==currency:
-                        amount = data["amount"]
-                        total_amount += amount
-
-                total_balance[prefix]= total_amount
-
-            print(total_balance)
-            
             # Update user balances using double-entry accounting
-            acc_balance = acc_doc.get('balance').get(currency)
-            print(acc_balance)
 
-            if total_balance[account]==acc_balance+amount:
-                acc_doc.reference.update({'balance.' + currency: acc_balance + amount})
-                account_transactions_ref.update({ "status": True})
-
-            return {'message': 'Money added in, lets ball'}, 201
+            return {'stripe_url': session.url, "sessionId": session["id"]}, 201
 
         except Exception as e:
             return {'error': str(e)}, 500
-
 
 
 class CreateTransaction(Resource):
@@ -184,14 +279,15 @@ class CreateTransaction(Resource):
 
             if not (currency in sender_doc.get("balance") and currency in receiver_doc.get("balance")):
                 return {'error': 'Sender or receiver does not have the currency selected'}, 404
-            
-            ## check whether the sender has enough money:
-            if sender_doc.get('balance').get(currency)<amount:
+
+            # check whether the sender has enough money:
+            if sender_doc.get('balance').get(currency) < amount:
                 return {'error': 'You dont have enough money, please top up'}, 404
             else:
                 pass
 
-            sender_transactions_ref = db.collection('transactions').document(sender+'-'+transaction_id)
+            sender_transactions_ref = db.collection(
+                'transactions').document(sender+'-'+transaction_id)
             sender_transactions_ref.set({
                 'transaction_acc': sender,
                 'receiver_user_id': sender,
@@ -199,11 +295,12 @@ class CreateTransaction(Resource):
                 'amount': -amount,  # Debit amount is negative
                 'currency': currency,
                 'type': 'credit',
-                'mode':'P2P',
-                'status':False
+                'mode': 'P2P',
+                'status': False
             })
 
-            receiver_transactions_ref = db.collection('transactions').document(receiver+'-'+transaction_id)
+            receiver_transactions_ref = db.collection(
+                'transactions').document(receiver+'-'+transaction_id)
             receiver_transactions_ref.set({
                 'transaction_acc': receiver,
                 'receiver_user_id': sender,
@@ -211,14 +308,15 @@ class CreateTransaction(Resource):
                 'amount': amount,  # Debit amount is negative
                 'currency': currency,
                 'type': 'debit',
-                'mode':'P2P',
-                'status':False
+                'mode': 'P2P',
+                'status': False
             })
 
-            prefix_to_search =[sender, receiver]
-            total_balance={}
+            prefix_to_search = [sender, receiver]
+            total_balance = {}
             for prefix in prefix_to_search:
-                query = db.collection('transactions').where("transaction_acc", "==", prefix )
+                query = db.collection('transactions').where(
+                    "transaction_acc", "==", prefix)
                 total_amount = 0
                 # Execute the query to retrieve matching documents
                 docs = query.stream()
@@ -226,24 +324,25 @@ class CreateTransaction(Resource):
                 for doc in docs:
                     data = doc.to_dict()
                     print(data)
-                    if "amount" in data and data['currency']==currency:
+                    if "amount" in data and data['currency'] == currency:
                         amount = data["amount"]
                         total_amount += amount
 
-                total_balance[prefix]= total_amount
-
+                total_balance[prefix] = total_amount
 
             # Update user balances using double-entry accounting
             sender_balance = sender_doc.get('balance').get(currency)
             receiver_balance = receiver_doc.get('balance').get(currency)
 
-            if total_balance[sender]==sender_balance-amount:
-                sender_doc.reference.update({'balance.' + currency: sender_balance - amount})
-                sender_transactions_ref.update({ "status": True})
+            if total_balance[sender] == sender_balance-amount:
+                sender_doc.reference.update(
+                    {'balance.' + currency: sender_balance - amount})
+                sender_transactions_ref.update({"status": True})
 
-            if total_balance[receiver]==receiver_balance+amount:
-                receiver_doc.reference.update({'balance.' + currency: receiver_balance + amount})
-                receiver_transactions_ref.update({ "status": True})
+            if total_balance[receiver] == receiver_balance+amount:
+                receiver_doc.reference.update(
+                    {'balance.' + currency: receiver_balance + amount})
+                receiver_transactions_ref.update({"status": True})
 
             return {'message': 'Transfer transaction created successfully'}, 201
         except Exception as e:
--- tik-tok-flask-api/flask_minimal/server.py
+++ tik-tok-flask-api/flask_minimal/server.py
@@ -10,9 +10,13 @@ api.add_resource(GetProfile, '/profile')
 api.add_resource(CreateTransaction, '/payment')
 api.add_resource(CreateUser, '/newUser')
 api.add_resource(TopUp, '/topUp')
+api.add_resource(CreateStripePayment, '/stripe')
+api.add_resource(stripe_webhook, '/webhook')
+
 
 def run_app(*args, **kwargs):
     app.run(*args, **kwargs, port=3000)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
